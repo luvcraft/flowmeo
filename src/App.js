1abdcfdba80377479a1230bcb116ab0813926a4e
R Hunter Gough <huntybunz@yahoo.com>
2022-07-17 16:36:42 -0600

    first commit, very much WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
+import {flowData} from './data';
+
+class Items extends React.Component {
+  renderItem(i) {
+    this.childArray = [];
+    flowData.map((data) => {
+      if(data.parents && data.parents.includes(i.id)){
+        this.childArray.push(data.id);
+      }
+    });
+
+    this.parents = i.parents ? i.parents.sort().join(" ") : "";
+    this.children = (this.childArray && this.childArray.length > 0) ? this.childArray.sort().join(" ") : ""
+
+    return (
+      <div className="item">
+        parents: {this.parents}<br />
+        ({i.id}) <b>{i.description}</b><br/>
+        children: {this.children}
+      </div>
+    );
+  }
+
+  render() {
+    const items = flowData.map((data, key) => {
+      return(
+        <li key={key}>
+          {this.renderItem(data)}
+        </li>
+      );
+    });
+
+    return (
+      <ol>{items}</ol>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Items />
     </div>
   );
 }
